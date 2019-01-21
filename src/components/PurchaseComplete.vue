e9ad3997a978bfc33ba6c75366099d5e39028eaf
white0221 <whiterm.r0221@gmail.com>
2019-01-21 20:18:17 +0900

    Add 確認画面

---
+++ src/components/PurchaseComplete.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="complete">
+    <p>User Name<br>
+    Thank you!</p>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '@/components/Home'
 import Wait from '@/components/Wait'
 import PurchaseConfirm from '@/components/PurchaseConfirm'
+import PurchaseComplete from '@/components/PurchaseComplete'
 
 Vue.use(Router)
 
@@ -22,6 +23,11 @@ export default new Router({
       path: '/confirm',
       name: 'Confirm',
       component: PurchaseConfirm
+    },
+    {
+      path: '/complete',
+      name: 'Complete',
+      component: PurchaseComplete
     }
   ]
 })
