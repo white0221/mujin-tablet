09afbb3c34b033f9cc94d9bdd7f6e5dac85bab7b
white0221 <whiterm.r0221@gmail.com>
2019-01-21 20:22:15 +0900

    Add 自己選択画面

---
+++ src/components/UserSelect.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="select">
+    User Select Page
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Home from '@/components/Home'
 import Wait from '@/components/Wait'
 import PurchaseConfirm from '@/components/PurchaseConfirm'
 import PurchaseComplete from '@/components/PurchaseComplete'
+import UserSelect from '@/components/UserSelect'
 
 Vue.use(Router)
 
@@ -28,6 +29,11 @@ export default new Router({
       path: '/complete',
       name: 'Complete',
       component: PurchaseComplete
+    },
+    {
+      path: '/select',
+      name: 'Select',
+      component: UserSelect
     }
   ]
 })
