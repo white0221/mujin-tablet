22102395ae3d1c573d4db8f154e354cf65629ee8
white0221 <whiterm.r0221@gmail.com>
2019-01-21 20:08:09 +0900

    Fixed 修正しやすく

---
+++ src/components/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="routes">
+    <p>リンク集</p>
+    <router-link to="/wait">Wait Page</router-link>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+p {
+  font-size: 20px;
+}
+</style>
