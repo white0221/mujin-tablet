83689413b34453d9c9015cd9752541260f5422bf
white0221 <whiterm.r0221@gmail.com>
2019-01-22 18:03:58 +0900

    Add ユーザリストを表示

--- src/components/UserSelect.vue
+++ src/components/UserSelect.vue
@@ -1,10 +1,26 @@
 <template>
   <div id="select">
-    User Select Page
+    Please select your name.
+    {{ info }}
   </div>
 </template>
 
 <script>
+export default {
+  name: "getUserList",
+  data() {
+    return {
+      url: "http://localhost:3000/tablet/user/list",
+      users: [],
+      info: null
+    }
+  },
+  mounted() {
+    axios
+      .get(this.url)
+      .then(response => (this.info = response))
+  }
+}
 </script>
 
 <style>
