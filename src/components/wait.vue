2cda4dd8c88c80a4b2cdfedf0aac59157768a1d3
white0221 <whiterm.r0221@gmail.com>
2019-01-24 13:57:36 +0900

    Create waitで受け取り体制

--- src/components/wait.vue
+++ src/components/wait.vue
@@ -5,6 +5,35 @@
 </template>
 
 <script>
+export default {
+  name: "wait",
+  data() {
+    return {
+      waitTime: 1,
+      interval: null,
+      url: "",
+      pool: null
+    }
+  },
+  created: function() {
+    let self = this
+    this.interval = setInterval(() =>{self.getPool()}, this.waitTime*1000);
+  },
+  methods: {
+    getPool: function() {
+      console.log("api access")
+      axios.get(this.url)
+          .then(response => (this.pool = response))
+          .catch(error => (this.pool = error))
+      if (this.pool != null) {
+        console.log(this.pool)
+        clearInterval(this.interval)
+        // document.location = "/#/confirm"
+        // confirmに遷移&confirmにデータ渡し
+      }
+    }
+  },
+}
 </script>
 
 <style>
