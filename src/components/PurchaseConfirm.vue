8ac74f0e037123a28119027f1b5104d12f776db8
white0221 <whiterm.r0221@gmail.com>
2019-01-24 22:32:34 +0900

    Comp 商品確認画面

--- src/components/PurchaseConfirm.vue
+++ src/components/PurchaseConfirm.vue
@@ -2,13 +2,16 @@
   <div id="confirm">
     <router-link to="/complete">OK</router-link>
     <router-link to="/select">NG</router-link>
-    <div>
+    <div id="head">
       <p>{{ purchase_info.user_name }}さん</p>
-      <p>￥{{ purchase_amount }} -</p>
+      <p class="red">￥{{ purchase_amount }} -</p>
+    </div>
+    <div id="body">
       <table>
+        <th>商品</th><th>単価</th><th>数量</th>
         <tr v-for="item in purchase_info.items">
           <td>{{ item.item_name }}</td>
-          <td>{{ item.value }}</td>
+          <td>￥{{ item.value }}</td>
           <td>{{ item.volume }}</td>
         </tr>
       </table>
@@ -35,4 +38,28 @@ export default {
 </script>
 
 <style>
+.red {
+  color: red;
+  font-size: 30px;
+}
+table {
+  border: 2px solid black;
+  border-radius: 3px;
+  background-color: #fff;
+  margin: auto;
+}
+
+th {
+  background-color: orange;
+  color: rgba(255,255,255,0.66);
+}
+
+td {
+  background-color: #f9f9f9;
+}
+
+th, td {
+  min-width: 120px;
+  padding: 10px 20px;
+}
 </style>
